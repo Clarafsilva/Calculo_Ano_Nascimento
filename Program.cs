d3e1d2ef1b6fc685ba6a3a5149954bf3636b2a7b
Sandra <Sandra@DESKTOP-671AQ2A>
2022-02-17 16:47:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Ano_Nascimento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            string Idade;
+
+
+            Console.WriteLine("Calculando o seu ano de nascimento:");
+
+            Console.Write("Escreva o seu nome: ");
+            Nome = Console.ReadLine();
+            Console.Write("Digite a sua idade: ");
+            Idade = Console.ReadLine();
+
+            int IdadeUsuario = int.Parse(Idade);
+            int CalcularAno = DateTime.Now.Year - IdadeUsuario;
+            Console.WriteLine("O ano (aproximado) do seu nascimento é: " + CalcularAno);
+            Console.ReadKey();
+            
+
+            
+
+
+
+
+            
+
+        }
+    }
+}
